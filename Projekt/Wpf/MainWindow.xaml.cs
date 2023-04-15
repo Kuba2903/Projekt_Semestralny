acce057d609b92271dccb5967311e671cbfd3519
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2023-04-15 20:03:11 +0200

    Created first window, connected with database

--- Projekt/Wpf/MainWindow.xaml.cs
+++ Projekt/Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,17 +26,108 @@ namespace Wpf
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Empty_strings()
         {
-            MainWindow mainWindow = new MainWindow();
-            Visibility = Visibility.Hidden;
-            mainWindow.Show();
+            firstname.Text = string.Empty;
+            surname.Text = string.Empty;
+            phone.Text = string.Empty;
+            data.Text = string.Empty;
+        }
+        int reader_id = 1;
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=HP;Initial Catalog=Book_Library;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Readers values (@first_name, @last_name,@phone_number,@return_date,@book_id)", con);
+            cmd.Parameters.AddWithValue("@first_name", firstname.Text);
+            cmd.Parameters.AddWithValue("@last_name", surname.Text);
+            cmd.Parameters.AddWithValue("@phone_number", phone.Text);
+            cmd.Parameters.AddWithValue("@return_date", data.Text);
+            cmd.Parameters.AddWithValue("@book_id", 1);
+
+            SqlCommand cmd2 = new SqlCommand("Insert into Rentals values (@book_id, @reader_id)", con);
+            cmd2.Parameters.AddWithValue("@book_id", 1);
+            cmd2.Parameters.AddWithValue("@reader_id", reader_id);
+
+
+            reader_id++;
+            cmd.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            con.Close();
+            Empty_strings();
+            MessageBox.Show("Success");
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click2(object sender, RoutedEventArgs e)
         {
-            frame.NavigationService.Navigate(new Uri("Authors.xaml", UriKind.Relative));
-            label.Visibility = Visibility.Hidden;
+            SqlConnection con = new SqlConnection("Data Source=HP;Initial Catalog=Book_Library;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Readers values (@first_name, @last_name,@phone_number,@return_date,@book_id)", con);
+            cmd.Parameters.AddWithValue("@first_name", firstname.Text);
+            cmd.Parameters.AddWithValue("@last_name", surname.Text);
+            cmd.Parameters.AddWithValue("@phone_number", phone.Text);
+            cmd.Parameters.AddWithValue("@return_date", data.Text);
+            cmd.Parameters.AddWithValue("@book_id", 2);
+
+            SqlCommand cmd2 = new SqlCommand("Insert into Rentals values (@book_id, @reader_id)", con);
+            cmd2.Parameters.AddWithValue("@book_id", 2);
+            cmd2.Parameters.AddWithValue("@reader_id", reader_id);
+            reader_id++;
+            cmd.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            con.Close();
+            Empty_strings();
+            MessageBox.Show("Success");
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=HP;Initial Catalog=Book_Library;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Readers values (@first_name, @last_name,@phone_number,@return_date,@book_id)", con);
+            cmd.Parameters.AddWithValue("@first_name", firstname.Text);
+            cmd.Parameters.AddWithValue("@last_name", surname.Text);
+            cmd.Parameters.AddWithValue("@phone_number", phone.Text);
+            cmd.Parameters.AddWithValue("@return_date", data.Text);
+            cmd.Parameters.AddWithValue("@book_id", 3);
+
+            SqlCommand cmd2 = new SqlCommand("Insert into Rentals values (@book_id, @reader_id)", con);
+            cmd2.Parameters.AddWithValue("@book_id", 3);
+            cmd2.Parameters.AddWithValue("@reader_id", reader_id);
+            reader_id++;
+            cmd.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            con.Close();
+            Empty_strings();
+            MessageBox.Show("Success");
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=HP;Initial Catalog=Book_Library;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Readers values (@first_name, @last_name,@phone_number,@return_date,@book_id)", con);
+            cmd.Parameters.AddWithValue("@first_name", firstname.Text);
+            cmd.Parameters.AddWithValue("@last_name", surname.Text);
+            cmd.Parameters.AddWithValue("@phone_number", phone.Text);
+            cmd.Parameters.AddWithValue("@return_date", data.Text);
+            cmd.Parameters.AddWithValue("@book_id", 4);
+
+            SqlCommand cmd2 = new SqlCommand("Insert into Rentals values (@book_id, @reader_id)", con);
+            cmd2.Parameters.AddWithValue("@book_id", 4);
+            cmd2.Parameters.AddWithValue("@reader_id", reader_id);
+            reader_id++;
+            cmd.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            con.Close();
+            Empty_strings();
+            MessageBox.Show("Success");
+        }
+
+        private void horrors_Selected(object sender, RoutedEventArgs e)
+        {
+            this.frame.Navigate(new Uri("Authors.xaml", UriKind.Relative));
         }
     }
 }
--- database_queries/create_Readers.sql
+++ database_queries/create_Readers.sql
@@ -5,8 +5,7 @@ reader_id int Primary Key Identity(1,1) not null,
 first_name nvarchar(30) not null,
 last_name nvarchar(30) not null,
 phone_number char(9) not null,
-book_id int not null Foreign Key References Books(book_id),
-return_date date check(return_date > getdate()) not null,
-penalty_amount smallmoney
+return_date date check(return_date > getdate() AND return_date < dateadd(m,3,getdate())) not null,
+book_id int not null Foreign Key References Books(book_id)
 )
 go
